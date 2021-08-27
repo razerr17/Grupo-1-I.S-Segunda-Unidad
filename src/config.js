c3b7bf45533479673fc2f5b8e20133052e89934f
jhom tacusi <67769710+jhontacusi@users.noreply.github.com>
2021-08-27 07:56:56 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ import { config } from "dotenv"
 config();
 export default {
     port: process.env.PORT || 4000,
-    dbUser: process.env.DB_USER || 'jhom',
-    dbPassword: process.env.DB_PASSWORD || 'cocodrilo10',
-    dbServer: process.env.DB_SERVER || 'LAPTOP-T0A9SMMC',
+    dbUser: process.env.DB_USER || '',
+    dbPassword: process.env.DB_PASSWORD || '',
+    dbServer: process.env.DB_SERVER || '',
     dbDatabase: process.env.DB_DATABASE || 'DBTutoria'
 };
